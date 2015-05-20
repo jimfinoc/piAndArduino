db504794ffc230fdcbcf78bbc949bdb105a58f0a
J <jimfinoc@gmail.com>
2015-05-19 21:22:22 -0400

    adding the listener

---
+++ serialListening.py
@@ -0,0 +1,31 @@
+import smbus
+import time
+import serial
+
+print "Press CTRL+Z to exit"
+
+class Zone(datetime.tzinfo):
+    def __init__(self,offset,isdst,name):
+        self.offset = offset
+        self.isdst = isdst
+        self.name = name
+    def utcoffset(self, dt):
+        return datetime.timedelta(hours=self.offset) + self.dst(dt)
+    def dst(self, dt):
+        return datetime.timedelta(hours=1) if self.isdst else datetime.timedelta(0)
+    def tzname(self,dt):
+        return self.name
+
+GMT = Zone(0,False,'GMT')
+# True if DST is on
+# Fales if now DST
+EST = Zone(-5,True,'EST')
+
+print datetime.datetime.utcnow().strftime('%m/%d/%Y %H:%M:%S %Z')
+print datetime.datetime.now(GMT).strftime('%m/%d/%Y %H:%M:%S %Z')
+print datetime.datetime.now(EST).strftime('%m/%d/%Y %H:%M:%S %Z')
+
+
+ser = serial.Serial('/dev/tty.usbserial', 9600)
+while True:
+    print ser.readline()
