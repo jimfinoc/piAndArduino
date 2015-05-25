2771cc0b190f87aec18d15314df752c8e5b61eab
J <jimfinoc@gmail.com>
2015-05-25 11:40:29 -0400

    processing the data to the webpage

--- serialListening.py
+++ serialListening.py
@@ -23,16 +23,32 @@ GMT = Zone(0,False,'GMT')
 # Fales if now DST
 EST = Zone(-5,True,'EST')
 
-print datetime.datetime.utcnow().strftime('%m/%d/%Y %H:%M:%S %Z')
+#print datetime.datetime.utcnow().strftime('%m/%d/%Y %H:%M:%S %Z')
 print datetime.datetime.now(GMT).strftime('%m/%d/%Y %H:%M:%S %Z')
 print datetime.datetime.now(EST).strftime('%m/%d/%Y %H:%M:%S %Z')
 
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 while True:
+    try:
+        #        localip = socket.gethostbyname(socket.gethostname())
+        localip = commands.getoutput("/sbin/ifconfig").split("\n")[9].split()[1][5:]
+    except:
+        localip = "No ip addr"
     currentTime = datetime.datetime.now(EST).strftime('%m/%d/%Y %H:%M:%S %Z')
     readData = json.loads(ser.readline())
     print currentTime,
     print json.dumps(readData)
-    print "Specific JSON data"
-    print readData['HTU21D-F Temperature']
+    print ""
+#    readData['HTU21D-F Temperature']
+#    readData['TSL2561 Luminosity']
+#    readData['HTU21D-F Humidity']
+    try:
+        with open('/var/www/index.html', 'w') as f:
+            tempInF = str(c_to_f(sensor2Data))
+            x = {"Local ip": localip, 'Local Time' : currentTime , 'Temperature' :     readData['HTU21D-F Temperature'] , "Brightness" :     readData['TSL2561 Luminosity'], "Location": "Indoor Green House"}
+            json.dump(x,f)
+            print x
+        f.closed
+    except:
+        print " cannot open file"
